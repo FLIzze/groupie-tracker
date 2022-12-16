e93bcc0f5d61e17166e1ade21424212238fc5bd8
llyse <lyse.lebourcq@ynov.com>
2022-12-16 16:30:19 +0100

    api

--- main.go
+++ main.go
@@ -6,53 +6,35 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
 
 type people struct {
-	id   int    `json:"Id"`
-	name string `json:"Name"`
+	Id   int    `json:"Id"`
+	Name string `json:"Name"`
 }
 
 func main() {
 	url := "https://groupietrackers.herokuapp.com/api/artists"
-
-	spaceClient := http.Client{
-		Timeout: time.Second * 2,
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		print(err)
 	}
-
-	req.Header.Set("User-Agent", "spacecount-tutorial")
-
-	res, getErr := spaceClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	if res.Body != nil {
-		defer res.Body.Close()
-	}
-
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
 		log.Fatal(readErr)
 	}
 
-	id1 := people{}
+	id1 := []people{}
 	jsonErr := json.Unmarshal(body, &id1)
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
 	}
 
-	fmt.Println(id1.name)
+	fmt.Println(id1)
 }
 
 //requestURL := "https://groupietrackers.herokuapp.com/api/artists"
-//request := new XMLHttpRequest()
+//request := := http.NewRequest()
 //contenu := requests.get(requestURL)
 //list := contenu.json()
 //request.send()
