0a464e0b42ef41f0047b1f9331308f3e3ddfa457
flizze <alexandre.bel@ynov.com>
2022-12-16 17:02:39 +0100

    api

--- server/server.go
+++ server/server.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+type people struct {
+	Id       int      `json:"Id"`
+	Name     string   `json:"Name"`
+	Status   string   `json:"Status"`
+	Image    string   `json:"Image"`
+	Members  []string `json:"Members"`
+	Location string   `json:"Location"`
+	First    string   `json:"First release"`
+	Creation string   `json:"Creation"`
+}
+
 func main() {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	res, err := http.Get(url)
+	if err != nil {
+		print(err)
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	text := []people{}
+	jsonErr := json.Unmarshal(body, &text)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	fmt.Println(text)
 	http.HandleFunc("/", Handler_index)
 	// //url of our funcs
 
