990a8312878828b4845a95f64aa3d2ac98b08d90
flizze <alexandre.bel@ynov.com>
2023-01-16 15:16:54 +0100

    major update

--- server/server.go
+++ server/server.go
@@ -23,7 +23,7 @@ var api people
 
 func main() {
 
-	http.HandleFunc("/", Handler_index)
+	http.HandleFunc("/", Handler_main)
 	// //url of our funcs
 
 	fs := http.FileServer(http.Dir("./static"))
@@ -34,7 +34,7 @@ func main() {
 	//listening on port 8080
 }
 
-func Handler_index(w http.ResponseWriter, r *http.Request) {
+func Handler_main(w http.ResponseWriter, r *http.Request) {
 	url := "https://groupietrackers.herokuapp.com/api/artists"
 	res, err := http.Get(url)
 	if err != nil {
